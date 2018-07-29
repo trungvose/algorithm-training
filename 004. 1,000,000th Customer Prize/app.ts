24474c256b9cc0c31f0aa9d52fdf875dcec328ca
Trung Vo <trungk18@gmail.com>
2018-07-29 22:38:25 +0800

    Update dynamic programing

--- 004. 1,000,000th Customer Prize/app.ts
+++ 004. 1,000,000th Customer Prize/app.ts
@@ -6,29 +6,33 @@ const maxVolume = 45 * 30 * 35;
 
 class Redmart {
     private static products: Product[] = [];
-
+    private static count = 0;
     public static Main(input: string[]) {
         this.readCsv(input);
         let wt = this.products.map(product => product.width * product.length * product.height);
         let val = this.products.map(product => product.price);
-        let result = this.knapstack(maxVolume, wt, val, this.products.length);
+        let result = this.knapSack(maxVolume, wt, val, this.products.length);
+        console.log(result);
     }
 
-    private static knapstack(W: number, wt: number[], val: number[], n: number) {
-        if (n === 0)
-            return 0;
-
-        // If volume of the nth item is more than Knapsack capacity W, then
-        // this item cannot be included in the optimal solution
-        if (wt[n - 1] > W) {
-            return this.knapstack(W, wt, val, n - 1);
+    private static knapSack(W: number, wt: number[], val: number[], n: number) {
+        let k: number[][] = [];
+        for (var i = 0; i <= n; i++) {
+            console.log(i);
+            k[i] = [];
+            for (var w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    k[i][w] = 0;
+                }
+                else if (wt[i - 1] <= w) {
+                    k[i][w] = Math.max(val[i - 1] + k[i - 1][w - wt[i - 1]], k[i - 1][w])
+                }
+                else {
+                    k[i][w] = k[i - 1][w];
+                }
+            }
         }
-        else {
-            // Return the maximum of two cases: 
-            // (1) nth item included 
-            // (2) not included  
-            return Math.max(val[n - 1] + this.knapstack(W - wt[n - 1], wt, val, n - 1), this.knapstack(W, wt, val, n - 1))
-        }
+        return k[n][W];
     }
 
     private static readCsv(input: string[]) {
@@ -41,7 +45,7 @@ class Redmart {
                 parseInt(width),
                 parseInt(height),
                 parseInt(weight));
-            if (product.length <= 45 || product.width <= 30 || product.height <= 35) {
+            if (product.length <= 45 && product.width <= 30 && product.height <= 35) {
                 this.products.push(product);
             }
         })
