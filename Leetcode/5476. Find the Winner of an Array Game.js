8ab7ca7a4ba7511478cc99b6fff73bdfb3eebef0
Trung Vo <trungk18@gmail.com>
2020-08-02 13:19:47 +0800

    Update solution

--- Leetcode/5476. Find the Winner of an Array Game.js
+++ Leetcode/5476. Find the Winner of an Array Game.js
@@ -7,29 +7,25 @@ var getWinner = function (arr, k) {
   if (k >= arr.length) {
     return Math.max(...arr);
   }
-  let winnerMap = new Map();
-  for (let i = 0; i < arr.length; i++) {
-    const first = arr[0];
-    const second = arr[1];
+  let tempK = 0;
+  let winner = arr[0];
+  while (tempK !== k) {
+    let first = arr[0];
+    let second = arr[1];
 
-    let winner = Math.max(first, second);
+    winner = Math.max(first, second);
 
     if (winner === first) {
       arr.splice(1, 1);
       arr.push(second);
+      tempK++;
     } else {
       arr.splice(0, 1);
       arr.push(first);
-    }
-
-    let winnerCount = winnerMap.get(winner);
-
-    winnerMap.set(winner, winnerCount ? winnerCount + 1 : 1);
-    i = 0;
-    if (winnerMap.get(winner) == k) {
-      return winner;
+      tempK = 1;
     }
   }
+  return winner;
 };
 
 export default getWinner;
