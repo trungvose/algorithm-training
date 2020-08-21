a3a3e20920a07f19ed18b9bcffb3f9d528d17060
Trung Vo <trungk18@gmail.com>
2020-08-21 11:52:20 +0800

    Intersection

---
+++ TypeScript/intersection.ts
@@ -0,0 +1,27 @@
+interface ErrorHandling {
+    success: boolean;
+    error?: { message: string };
+  }
+  
+  interface ArtworksData {
+    artworks: { title: string }[];
+  }
+  
+  interface ArtistsData {
+    artists: { name: string }[];
+  }
+  
+  // These interfaces are composed to have
+  // consistent error handling, and their own data.
+  
+  type ArtworksResponse = ArtworksData & ErrorHandling;
+  type ArtistsResponse = ArtistsData & ErrorHandling;
+  
+  const handleArtistsResponse = (response: ArtistsResponse) => {
+    if (response.error) {
+      console.error(response.error.message);
+      return;
+    }
+  
+    console.log(response.artists);
+  };
