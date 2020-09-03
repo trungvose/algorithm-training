564cff66190f8f5779ac8db23827015ee8d9a666
Trung Vo <trungk18@gmail.com>
2020-09-03 11:00:25 +0800

    674. Longest Continuous Increasing Subsequence

---
+++ Leetcode/674. Longest Continuous Increasing Subsequence.ts
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+function findLengthOfLCIS(nums: number[]): number {
+  if (!nums.length) {
+    return 0;
+  }
+  
+  let length = 1;
+  let max = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    const current = nums[i];
+    const next = nums[i + 1];
+    if (current < next) {
+      length++;
+    } else {
+      max = max < length ? length : max;
+      length = 1;
+    }
+  }
+  return Math.max(max, length);
+}
