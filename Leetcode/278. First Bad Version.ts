8ed027be8f2271b1c5c1ac14c9fa0841f005deb0
Trung Vo <trungk18@gmail.com>
2020-09-07 22:40:35 +0800

    First bad version - brute force and BST

---
+++ Leetcode/278. First Bad Version.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+//Time O(logn)
+//Space O(1)
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 1;
+    let right = n;
+    while (left < right) {
+      let mid = Math.floor(left + (right - left) / 2);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
+  };
+};
