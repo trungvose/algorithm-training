8bc1c7ba2c937aaee474bd29969c7634b5bd7cd5
trungk18 <trungk18@hotmail.com>
2020-09-14 09:17:17 +0800

    67. Add Binary

---
+++ Leetcode/67. Add Binary.test.ts
@@ -0,0 +1,7 @@
+import { addBinary } from "./67. Add Binary";
+
+describe("67. Add Binary.ts", () => {
+  it("should pass the first test", () => {
+    expect(addBinary("1010", "1011")).toBe("10101");
+  });
+});
