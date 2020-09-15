000a2c0cd6e31825bcf8c53f0161402f1b541334
trungk18 <trungk18@hotmail.com>
2020-09-15 08:35:07 +0800

    200. Number of Islands

---
+++ Leetcode/200. Number of Islands.test.ts
@@ -0,0 +1,25 @@
+import { numIslands } from "./200. Number of Islands";
+
+describe("200. Number of Islands", () => {
+  it("should pass the Test case 01", () => {
+    expect(
+      numIslands([
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"],
+      ])
+    ).toBe(1);
+  });
+
+  it("should pass the Test case 02", () => {
+    expect(
+      numIslands([
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"],
+      ])
+    ).toBe(3);
+  });
+});
---
+++ Leetcode/200. Number of Islands.ts
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/number-of-islands
+
+export function numIslands(grid: string[][]): number {
+  if (!grid.length) {
+    return 0;
+  }
+
+  let rows = grid.length;
+  let cols = grid[0].length;
+  let numOfIslands = 0;
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === "1") {
+        numOfIslands++;
+        dfs(grid, r, c);
+      }
+    }
+  }
+
+  return numOfIslands;
+
+  function dfs(grid: string[][], r: number, c: number) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let isNotValidOrOutsideOfGrid =
+      r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === "0";
+    if (isNotValidOrOutsideOfGrid) {
+      return;
+    }
+
+    grid[r][c] = "0";
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+}
