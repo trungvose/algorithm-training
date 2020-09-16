bf83db4a7f2abd9ff4c0aadd68a1cf3ab4d559cd
trungk18 <trungk18@gmail.com>
2020-09-16 09:31:13 +0800

    Friend circles DFS

---
+++ Leetcode/547. Friend Circles.test.ts
@@ -0,0 +1,23 @@
+import { findCircleNum } from "./547. Friend Circles";
+
+describe("547. Friend Circles", () => {
+  it("should pass the Test case 01", () => {
+    expect(
+      findCircleNum([
+        [1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1],
+      ])
+    ).toBe(2);
+  });
+
+  it("should pass the Test case 02", () => {
+    expect(
+      findCircleNum([
+        [1, 1, 0],
+        [1, 1, 1],
+        [0, 1, 1],
+      ])
+    ).toBe(1);
+  });
+});
---
+++ Leetcode/547. Friend Circles.ts
@@ -0,0 +1,22 @@
+export function findCircleNum(M: number[][]): number {
+  let count = 0;
+  for (let i = 0; i < M.length; i++) {
+    if (M[i][i] === 1) {
+      count++;
+      dfs(M, i);
+    }
+  }
+  return count;
+  
+  function dfs(M: number[][], v: number) {
+    if (M[v][v] === 0) {
+      return;
+    }
+    for (let i = 0; i < M.length; i++) {
+      if (M[v][i] === 1) {
+        M[v][i] = 0;
+        dfs(M, i);
+      }
+    }
+  }
+}
