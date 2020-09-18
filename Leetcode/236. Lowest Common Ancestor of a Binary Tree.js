dfabc3b09aa3442513342bab90d24decef9bbc54
trungk18 <trungk18@gmail.com>
2020-09-18 09:24:29 +0800

    236. Lowest Common Ancestor of a Binary Tree Recursive

---
+++ Leetcode/236. Lowest Common Ancestor of a Binary Tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+//https://www.youtube.com/watch?v=13m9ZCB8gjw
+
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root) {
+    return null;
+  }
+
+  if (root === p || root === q) {
+    return root;
+  }
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) {
+    return root;
+  }
+
+  if (!left && !right) {
+    return null;
+  }
+
+  return left || right;
+};
