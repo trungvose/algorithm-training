7ef2e0ce982d7f9c540aa52f481beb09bafe0a25
Trung Vo <trungk18@gmail.com>
2020-09-28 21:24:36 +0800

    399. Evaluate Division

---
+++ 399. Evaluate Division.test.ts
@@ -0,0 +1,22 @@
+import { calcEquation } from "./399. Evaluate Division";
+
+describe("399. Evaluate Division", () => {
+  it("should pass the first test case", () => {
+    expect(
+      calcEquation(
+        [
+          ["a", "b"],
+          ["b", "c"],
+        ],
+        [2.0, 3.0],
+        [
+          ["a", "c"],
+          ["b", "a"],
+          ["a", "e"],
+          ["a", "a"],
+          ["x", "x"],
+        ]
+      )
+    ).toMatchObject([6.0, 0.5, -1.0, 1.0, -1.0]);
+  });
+});
