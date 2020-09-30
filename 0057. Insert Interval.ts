169d13884de149f56ab04be24ca1b31d647540dc
Trung Vo <trungk18@gmail.com>
2020-09-30 12:30:13 +0800

    Remove extra array

--- 0057. Insert Interval.ts
+++ 0057. Insert Interval.ts
@@ -1,6 +1,5 @@
 import { merge } from "./0056. Merge Intervals";
 
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-  let combinedIntervals = [...intervals, newInterval];
-  return merge(combinedIntervals);
+  return merge([...intervals, newInterval]);
 }
