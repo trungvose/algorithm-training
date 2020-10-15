4cd2e25ca1c9887e6ad42988a9d7dcf94683b286
Trung Vo <trungk18@gmail.com>
2020-10-15 22:15:42 +0800

    238. Product of Array Except Self O(n^2)

---
+++ 0238. Product of Array Except Self.ts
@@ -0,0 +1,8 @@
+function productExceptSelf(nums: number[]): number[] {
+  return nums.map((num, idx) => nums.reduce((product, n, i) => {
+    if(i === idx){
+      return product;
+    }
+    return product * n;
+  }, 1));
+}
