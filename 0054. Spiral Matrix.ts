6ca535b1c6275f92610d5931972bebe4bb7e8046
Trung Vo <trungk18@gmail.com>
2020-10-19 09:55:31 +0800

    54. Spiral Matrix pass all test case

--- 0054. Spiral Matrix.ts
+++ 0054. Spiral Matrix.ts
@@ -7,49 +7,28 @@ export function spiralOrder(matrix: number[][]): number[] {
   let R = matrix[0].length - 1;
   let T = 0;
   let B = matrix.length - 1;
-  let direction = Direction.LeftToRight;
   let out: number[] = [];
 
   while (T <= B && L <= R) {
-    if (direction === Direction.LeftToRight) {
-      for (let i = L; i <= R; i++) {
-        out.push(matrix[T][i]);
-      }
-      T++;
-      direction = Direction.TopToBottom;
+    for (let i = L; i <= R; i++) {
+      out.push(matrix[T][i]);
     }
-
-    if (direction === Direction.TopToBottom) {
-      for (let i = T; i <= B; i++) {
-        out.push(matrix[i][R]);
-      }
-      R--;
-      direction = Direction.RightToLeft;
+    for (let i = T + 1; i <= B; i++) {
+      out.push(matrix[i][R]);
     }
-
-    if (direction === Direction.RightToLeft) {
-      for (let i = R; i >= L; i--) {
+    if(T < B && L < R){
+      for (let i = R -1; i > L; i--) {
         out.push(matrix[B][i]);
       }
-      B--;
-      direction = Direction.BottomToTop;
-    }
-
-    if (direction === Direction.BottomToTop) {
-      for (let i = B; i >= T; i--) {
+      for (let i = B; i > T; i--) {
         out.push(matrix[i][L]);
       }
-      L++;
-      direction = Direction.BottomToTop;
     }
+    T++;
+    B--;
+    L++;
+    R--;
   }
 
   return out;
-}
-
-enum Direction {
-  LeftToRight = 0,
-  TopToBottom = 1,
-  RightToLeft = 2,
-  BottomToTop = 3,
 }
