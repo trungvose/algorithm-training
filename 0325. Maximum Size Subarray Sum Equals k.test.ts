116d3fec94deb3ab128c858c40885d81268adf81
Trung Vo <trungk18@gmail.com>
2020-10-22 21:22:50 +0800

    325. Maximum Size Subarray Sum Equals k

---
+++ 0325. Maximum Size Subarray Sum Equals k.test.ts
@@ -0,0 +1,6 @@
+import { maxSubArrayLen } from './0325. Maximum Size Subarray Sum Equals k';
+
+describe('0325. Maximum Size Subarray Sum Equals k', () => {
+  //expect(maxSubArrayLen([1, -1, 5, -2, 3], 3)).toBe(4);
+  expect(maxSubArrayLen([-1], -1)).toBe(1);
+});
