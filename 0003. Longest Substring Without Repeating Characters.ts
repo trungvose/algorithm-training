729acd86674038881c1c097178717b84422ad8a4
Trung Vo <trungk18@gmail.com>
2020-11-05 08:03:58 +0800

    3. Longest Substring Without Repeating Characters

---
+++ 0003. Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,17 @@
+// "abcabcbb"
+
+function lengthOfLongestSubstring(s: string): number {
+  let output = 0;
+  let current: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (current.includes(char)) {
+      current.splice(current.indexOf(char));
+    }
+    current.unshift(char);
+    if (current.length > output) {
+      output = current.length;
+    }
+  }
+  return output;
+}
